c7a66bcfebeec446a2196a55f3d31924c1a195ed
EKaynar67 <68536228+EKaynar67@users.noreply.github.com>
2024-10-14 09:43:48 +0300

    Create p3_.cs

---
+++ p3_.cs
@@ -0,0 +1,66 @@
+// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+/*
+
+ - do not use Turkish chars
+ - use meaningful variable names 
+ - var names mustnt start with numbers however var names could have numbers
+ - do not use uniqe symbols except "_" and "@ for abstract classes"
+ - don't use space in variable name
+ - commands which are belong to programming language can not be used a variable name
+ - upper and lower letter sensvity is exist
+ - variables which have same name can not be used in the same code block
+
+ */
+
+namespace NetApp
+{
+    internal class Program
+    {
+        static int distanceCalculator(int x, int y, int z)
+        {
+            int result = x + y + z;
+           
+            
+            //
+
+            Console.Write($"Gideceğiniz toplam mesafe: {x + y + z}");
+
+            return result;
+            //Console.Write(x + y + z);
+        }
+       
+        static void Main(string[] args)
+        {
+            int x = 0; int y = 0; int z = 0;
+
+            Console.WriteLine("Nevşehir ile Kayseri arasındaki mesafeyi giriniz");
+            x = Convert.ToInt32(Console.ReadLine());
+            //
+            Console.WriteLine("Kayseri ile Adana arasındaki mesafeyi giriniz");
+            y = Convert.ToInt32(Console.ReadLine());
+            //
+            Console.WriteLine("Adana ile Mersin arasındaki mesafeyi giriniz");
+            z = Convert.ToInt32(Console.ReadLine());
+
+
+            distanceCalculator(x,y,z);
+
+            //distanceCalculator(n_k, k_a, a_m);
+            //y = Convert.ToInt32(Console.ReadLine());
+
+            Console.ReadKey();
+            
+        }
+    }
+}
+
+
+// bir kişi şehirler arası seyehat planı yapmakta
+// kişi nevşehirden önce kayseriye ardından kayseriden adanaya son olarak da adanadan mersine gitmek istedi. buna göre kullanıcıya gideceği şehirler arası mesafeyi sorup toplamda gideceği mesafeyi hesaplayan ve ekrana yazan programı yazın.
